850dac874727758b7caa3e835a89411822d02313
pedro.escobar <pedro.escobar@MEXL002218>
2021-09-04 16:54:14 -0500

    Second commit

--- src/main/java/com/stacksimplify/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/stacksimplify/SpringbootBuildingblocksApplication.java
@@ -8,7 +8,7 @@ public class SpringbootBuildingblocksApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBuildingblocksApplication.class, args);
-		//First comment in code 
+		//First comment in code  
 	}
 
 }
